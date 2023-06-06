4af1e1d2de6bc24c2ce127d29673c78ea1f8f253
erasmodominguezdc <erasmo_dominguez@discovery.com>
2023-06-06 14:35:26 +0100

    multiples images

--- test.rb
+++ test.rb
@@ -1,10 +1,7 @@
 require 'fluent-logger'
 
-# Configura el logger de Fluentd para enviar eventos a localhost en el puerto 24224
 logger = Fluent::Logger::FluentLogger.new(nil, host: 'localhost', port: 24224)
 
-# Envía un evento de prueba al flujo de datos
 logger.post('myapp.access', { 'message' => 'This is a test log message' })
 
-# Cierra la conexión del logger de Fluentd
 logger.close
